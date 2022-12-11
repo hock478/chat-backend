347575b28bf2c8925d88d93333d49d01d6bfab50
Brook Nega <brooknega21@gmail.com>
2022-12-11 15:43:10 -0500

    server stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,6 @@
 Rails.application.routes.draw do
-  resources :follows
-  resources :messages
-  resources :groups
-  resources :group_users
-  resources :users
-  resources :likes
-  resources :posts
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  post '/login', to: 'auth#create'
-  get '/decode_token', to: 'users#profile'
-  get '/specific/:username', to: 'users#specific'
-  get '/following_posts/:id', to: 'posts#following_posts'
-  post '/like_destroy', to: 'likes#like_destroy'
-  post '/delete_fol', to: 'follows#delete_fol'
-  post '/delete_user', to: 'users#delete_user'
-
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -2,53 +2,52 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_17_184436) do
-
+ActiveRecord::Schema[7.0].define(version: 2020_06_17_184436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "follows", force: :cascade do |t|
     t.integer "user_id"
     t.integer "following_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "group_users", force: :cascade do |t|
     t.integer "user_id"
     t.integer "group_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "groups", force: :cascade do |t|
     t.string "title"
     t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "likes", force: :cascade do |t|
     t.string "user_id"
     t.string "post_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "messages", force: :cascade do |t|
     t.integer "user_id"
     t.integer "group_id"
     t.string "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
@@ -56,8 +55,8 @@ ActiveRecord::Schema.define(version: 2020_06_17_184436) do
     t.string "header"
     t.string "content"
     t.text "hash_tags", default: [], array: true
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -67,8 +66,8 @@ ActiveRecord::Schema.define(version: 2020_06_17_184436) do
     t.integer "age"
     t.string "bio"
     t.string "profile_pic"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
